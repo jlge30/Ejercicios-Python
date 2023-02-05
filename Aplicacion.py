7db277ecd043c87e353741412c626472467a0cf4
jlge30 <jlge30@gmail.com>
2023-02-05 16:31:43 +0100

    Actualizado

--- Aplicacion.py
+++ Aplicacion.py
@@ -30,5 +30,24 @@ milista.append("Juana")#añade elementos
 print(milista)
 milista.insert(3,"Raul")#añade elementos en una posición concreta
 print(milista)
-milista.extend(["Antonio","Felipe","Clara"])# añadir más listas
+milista.extend(["Antonio","Raul","Clara"])# añadir más listas
 print(milista)
+print(milista.index("Raul"))# nos devuelve el primer indice donde está un valor
+print("Pepe" in milista)# nos devuelve verdadero o falso, busca valores en la búsqueda
+milista.extend([False, 2])# se pueden añadir elementos de difernte tipo a la misma lista
+print(milista)
+milista.remove("Raul")#elimina elementos de una lista
+print(milista)
+milista.remove("Raul")
+print(milista)
+milista.pop()#elimina el último elemento de una lista
+print(milista)
+milista1= ["Juan","Santos","Con","Yo"]
+milista3 = milista + milista1#juntamos listas con el más
+print(milista3)
+milista1= ["Juan","Santos","Con","Yo"]*3 # multiplico la lista por 3
+print(milista1)
+#Tuplas no se pueden mover dentro de las mismas
+mitupla = ("hola", "Pedro","Joaquin",12,True)
+print(mitupla)
+print(mitupla[2])#escojo un elemento
