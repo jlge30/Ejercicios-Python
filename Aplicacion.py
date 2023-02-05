8900f1863c51ca5488a6f5cc715156472da88e86
jlge30 <jlge30@gmail.com>
2023-02-05 15:56:47 +0100

    Actualizado

--- Aplicacion.py
+++ Aplicacion.py
@@ -1,1 +1,23 @@
-print("holafdas mundo")
+#funciones
+def imprimir():
+    print("holafdas mundo")
+    print("holafdas mundo")
+    print("holafdas mundo")
+
+
+imprimir()
+imprimir()
+num1 = 5
+num2 = 7
+print(num1 + num2)
+
+def sumar( a, b):
+    c = a + b
+    print(c)
+
+sumar ( 8, 10)
+sumar ( 25, 10)
+sumar ( 8, 43)
+
+
+    
