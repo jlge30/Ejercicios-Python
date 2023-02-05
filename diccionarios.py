3998449e8a1258da597d49e9fe860be1a155db11
jlge30 <jlge30@gmail.com>
2023-02-05 23:30:07 +0100

    añado campos

---
+++ diccionarios.py
@@ -0,0 +1,7 @@
+midiccionario = {"Alemana": "Berlin", "Espagna": "Madrid", "Inglaterra":"Londres"}#creados de diccionarios
+print(midiccionario["Espagna"])
+print(midiccionario)
+midiccionario["Italia"]="Barcelona"#añado un elemento al diccionario
+print(midiccionario)
+midiccionario ["Italia"] = "Roma"#modifico el diccionario
+print(midiccionario)
