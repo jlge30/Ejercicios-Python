1ca6cb4fb067a218451e186542dd730e618d00f4
jlge30 <jlge30@gmail.com>
2023-02-10 00:06:56 +0100

    actul.

--- condicionales2.py
+++ condicionales2.py
@@ -21,6 +21,31 @@ else:
     print("Sobresaliente")
 
 
+edad2 = int(input("Introduce tu edad"))
+
+if edad2 < 100 and edad2 > 0: 
+    print("Edad correcto")
+else: 
+    print("Edad incorrecta")
+
+
+salario_presidente = int(input("Introduce salario del presidente"))
+print("Salario del presidente", str(salario_presidente))
+salario_director = int(input("Introduce salario del director"))
+print("Salario del director", str(salario_director))
+salario_jefe_area = int(input("Introduce salario del jefe de area"))
+print("Salario del jefe de area", str(salario_jefe_area))
+salario_administrativo = int(input("Introduce salario del administrativo"))
+print("Salario del administrativo", str(salario_administrativo))
+
+if salario_administrativo < salario_jefe_area < salario_director < salario_director:
+    print("Todo está correcto")
+else:
+    print("Algo falla en la empresa")
+
+input("Programa Becas")
+
+
 
 
 
