e66fe7d669393ac4e5703245766d5bf31eada306
jlge30 <jlge30@gmail.com>
2023-02-19 14:08:28 +0100

    Actualizacion

--- bucles.py
+++ bucles.py
@@ -1,4 +1,9 @@
 for i in ["Primavera", "Verano", "Otoño", "Invierno"]:
     print(i)
 
-    
+for i in ["Primavera", "Verano", "Otoño", "Invierno"]:
+    print(i, end = "")# lo imprime todo en la misma linea sin espacios
+
+print()
+for i in ["Primavera", "Verano", "Otoño", "Invierno"]:
+    print(i, end = " ")# lo imprime todo en la misma linea con espacios
